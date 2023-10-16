e451bdf35181088cd37e73740f3dcdb48d6fe6c7
Nihylin <nihylin@gmail.com>
2023-10-16 16:10:03 +0200

    Fix 'alphABET' program

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -3,7 +3,7 @@
 /**
 * main - Entry point
 *
-* Description: prints the alphabet in lowercase, and then in 
+* Description: prints the alphabet in lowercase, and then in
 * uppercase, followed by a new line.
 *
 * Return: Always 0 (Success)
@@ -17,10 +17,10 @@ int main(void)
 	{
 		putchar (c);
 	}
-	putchar ('\n');
 	for (c = 'A' ; c <= 'Z' ; c++)
 	{
 		putchar (c);
 	}
+	putchar ('\n');
 	return (0);
 }
