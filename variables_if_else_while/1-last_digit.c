fcf1b13432ccbc50c9f7c5faef710f4c3ae4c307
Nihylin <nihylin@gmail.com>
2023-10-16 14:31:14 +0200

    Add 'The last digit' C program

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: print the last digit of the number stored in the variable n.
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	int n, lstdgt;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lstdgt = n % 10;
+	if (lstdgt > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lstdgt);
+	}
+	else if (lstdgt < 6 && lstdgt != 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lstdgt);
+	}
+	else if (lstdgt == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lstdgt);
+	}
+	return (0);
+}
