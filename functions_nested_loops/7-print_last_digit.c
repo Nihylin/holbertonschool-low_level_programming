a62c7189a4044b1569520caa0cce7bdab98763ef
Nihylin <nihylin@gmail.com>
2023-10-17 22:51:23 +0200

    Fix 'There are only 3...' C program

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -16,6 +16,13 @@ int print_last_digit(int i)
 		i = i * -1;
 	}
 	i = i % 10;
-	_putchar (i);
+	_putchar (i + 48);
+	/*
+	* Cette ligne imprime le dernier chiffre de i.
+	* Cela fonctionne en ajoutant 48 (la valeur ASCII de '0') à i.
+	* Ainsi, si i est par exemple 3, i + 48 donnera 51,
+	* ce qui correspond au caractère ASCII '3'.
+	* Le caractère est ensuite affiché à l'aide de la fonction _putchar.
+	*/
 	return (i);
 }
