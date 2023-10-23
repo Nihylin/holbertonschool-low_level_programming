42b2dfe17bf8442e6d4e610d46124d611b92ea4d
Nihylin <nihylin@gmail.com>
2023-10-23 15:28:38 +0200

    Add '98 Battery st.' C program

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value of a pointer to 98
+ * @n: the pointer being updated and returning 98
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Lorem Ipsum tes grands morts
