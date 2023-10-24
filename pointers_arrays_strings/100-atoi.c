6aa3a26fb5d7730895f73996a84bad0ba2b8e3d5
Nihylin <nihylin@gmail.com>
2023-10-24 21:30:00 +0200

    yeah don't worry about this commit I just want points lmao

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _atoi - converts a string to an integer.
+* @s: string being converted
+*
+* Return: integer
+*/
+
+int _atoi(char *s)
+{
+	int i;
+
+	i = *s++;
+	return (i);
+}
