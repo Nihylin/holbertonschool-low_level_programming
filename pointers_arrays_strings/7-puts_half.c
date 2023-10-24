ad6f2d784cec66ee64b23866af646681f0b82997
Nihylin <nihylin@gmail.com>
2023-10-24 16:53:18 +0200

    Add 'Winning is only half...' C program

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: string being cut in half
+ *
+ * Return: nothing (void)
+ */
+
+void puts_half(char *str)
+{
+	int i = 0;
+	int length;
+	int n;
+
+	for (length = 0; str[length] != '\0'; length++)
+		;
+
+	if (length % 2 == 0)
+	{
+		for (i = length / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (n = (length + 1) / 2; str[n] != '\0'; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	_putchar('\n');
+}
