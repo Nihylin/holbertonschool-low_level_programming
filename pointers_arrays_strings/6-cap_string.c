d4358cf6c750e8777f25d01326d144dfb117a9a2
Nihylin <nihylin@gmail.com>
2023-10-27 11:16:22 +0200

    Fix 'Expect the best...' C program

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -22,25 +22,17 @@ char *cap_string(char *str)
 			continue;
 		}
 		if (
-		(str[i] == ' ') || (str[i] == '\t') || (str[i] == '\n') ||
-		(str[i] == ',') || (str[i] == ';') || (str[i] == '.') ||
-		(str[i] == '!') || (str[i] == '?') || (str[i] == '"') ||
-		(str[i] == '(') || (str[i] == ')') || (str[i] == '{') ||
-		(str[i] == '}')
+		(str[i - 1] == ' ') || (str[i - 1] == '\t') || (str[i - 1] == '\n') ||
+		(str[i - 1] == ',') || (str[i - 1] == ';') || (str[i - 1] == '.') ||
+		(str[i - 1] == '!') || (str[i - 1] == '?') || (str[i - 1] == '"') ||
+		(str[i - 1] == '(') || (str[i - 1] == ')') || (str[i - 1] == '{') ||
+		(str[i - 1] == '}')
 		)
 		{
-			i++;
 			if (str[i] >= 'a' && str[i] <= 'z')
 			{
 				str[i] = str[i] - 32;
 				continue;
-			}
-		}
-		else
-		{
-			if (str[i] >= 'A' && str[i] <= 'Z')
-			{
-				str[i] = str[i] + 32;
 			}
 		}
 	}
