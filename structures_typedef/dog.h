3119cc2e7fd27c80572831cccbf80492872cfe62
Nihylin <nihylin@gmail.com>
2023-11-13 16:09:49 +0100

    Add 'A door is what a dog...' C program

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -6,7 +6,6 @@
  * @name: name of the puppy
  * @age: age the good boye
  * @owner: the lucky owner
- * Return: nothing (void)
 */
 
 struct dog
@@ -20,5 +19,6 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
