ce60fa5bec94b81fcad2973580a04e17cb1a0c76
Nihylin <nihylin@gmail.com>
2023-11-13 13:48:37 +0100

    Add 'Poppy' header file

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - woof
+ * @name: name of the puppy
+ * @age: age the good boye
+ * @owner: the lucky owner
+ * Return: nothing (void)
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
