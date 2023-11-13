9b5b92ba7dfada3f3fbf3f8b9d2175195d13531a
Nihylin <nihylin@gmail.com>
2023-11-13 14:50:36 +0100

    Add 'A dog will teach you...' C program

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: identifier
+ * Return: nothing (void)
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+	{
+		return;
+	}
+	else if (d->name == NULL)
+	{
+		d->name = "(nil)";
+	}
+
+	else if (d->owner == NULL)
+	{
+		d->owner = "(nil)";
+	}
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
