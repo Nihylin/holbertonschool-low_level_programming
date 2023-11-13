9b3c0dfcba5a860d6b23afbd2340b729cd90fad0
Nihylin <nihylin@gmail.com>
2023-11-13 16:14:08 +0100

    hotfix memory leak 'A door is what a dog...' C program

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -25,6 +25,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if ((dog->name == NULL) || (dog->owner == NULL))
 	{
+		free(dog->name);
+		free(dog->owner);
 		free(dog);
 		return (NULL);
 	}
