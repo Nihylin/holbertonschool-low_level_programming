8f8ea1c57fa9dbab9167bc114c30b6163f0a3de1
Nihylin <nihylin@gmail.com>
2023-12-06 15:03:45 +0100

    fix T2 errors return values

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -14,14 +14,14 @@ int append_text_to_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 	{
-		return (0);
+		return (-1);
 	}
 
 	file = open(filename, O_RDWR | O_APPEND);
 
 	if (file < 0)
 	{
-		return (0);
+		return (-1);
 	}
 
 	if (text_content != NULL)
